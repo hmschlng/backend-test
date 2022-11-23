19f50a9222dab42a3e52ea11e153a847a90c492c
hmschlng <soulb9@naver.com>
2022-11-23 17:54:23 +0900

    modify MemberController

--- backtest/src/main/java/com/ssafy/backtest/member/controller/MemberController.java
+++ backtest/src/main/java/com/ssafy/backtest/member/controller/MemberController.java
@@ -1,96 +1,91 @@
-//package com.ssafy.backtest.member.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.ssafy.backtest.member.model.dto.Member;
-//import com.ssafy.backtest.member.model.service.MemberService;
-//import com.ssafy.backtest.util.MailService;
-//
-//@RestController
-//@CrossOrigin
-//@RequestMapping("member")
-//public class MemberController {
-//
-//	@Autowired
-//	private MemberService memberService;
+package com.ssafy.backtest.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.backtest.member.model.dto.Member;
+import com.ssafy.backtest.member.model.service.MemberService;
+import com.ssafy.backtest.util.MailService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("member")
+public class MemberController {
+
+	@Autowired
+	private MemberService memberService;
+	
+	@Autowired
+	private MailService mailService;
 //	
-//	@Autowired
-//	private MailService mailService;
-////	
-////	@GetMapping("/logout")
-////	public ResponseEntity<String> logout() throws Exception {
-////		return null;
-////	}
-////	
-////	@PostMapping("/login")
-////	public ResponseEntity<String> login(
-////			@RequestBody Member member
-////			) throws Exception {
-////		return null;
-////	}
-////	
-//
-////	
-////	@PutMapping("/update")
-////	public ResponseEntity<String> updateMember(
-////			@RequestBody Member member
-////			) throws Exception {
-////		return null;
-////	}
-////	
-////	@DeleteMapping("/{emailId}")
-////	public ResponseEntity<String> deleteMember(
-////			) throws Exception {
-////		return null;
-////	}
+//	@GetMapping("/logout")
+//	public ResponseEntity<String> logout() throws Exception {
+//		return null;
+//	}
 //	
-//
-//	
-//	@PostMapping("/join")
-//	public ResponseEntity<String> join(
+//	@PostMapping("/login")
+//	public ResponseEntity<String> login(
 //			@RequestBody Member member
 //			) throws Exception {
-//		if(memberService.joinMember(member)) {
-//			return new ResponseEntity<String>("success", HttpStatus.OK);
-//		}
-//		return new ResponseEntity<String>("fail", HttpStatus.BAD_REQUEST);
+//		return null;
 //	}
 //	
-//	@PostMapping("check")
-//	public ResponseEntity<?> checkEmailId(@RequestBody Member member) throws Exception {
-//		return new ResponseEntity<String>(memberService.checkEmailId(member), HttpStatus.OK);
-//	}
+
 //	
-//	@GetMapping("{emailId}")
-//	public ResponseEntity<?> searchPw(
-//			@PathVariable String emailId
-//			) throws Exception {
-//		return new ResponseEntity<Boolean>(memberService.searchPw(emailId), HttpStatus.OK);
-//	}
-//	
-//	@PostMapping
-//	public ResponseEntity<?> login(
+//	@PutMapping("/update")
+//	public ResponseEntity<String> updateMember(
 //			@RequestBody Member member
 //			) throws Exception {
-//		return ResponseEntity.ok().body(memberService.login(member));
+//		return null;
 //	}
 //	
-//	@PutMapping("updatePw")
-//	public ResponseEntity<?> updatePw(
-//			@RequestBody Member member
+//	@DeleteMapping("/{emailId}")
+//	public ResponseEntity<String> deleteMember(
 //			) throws Exception {
-//		return ResponseEntity.ok().body(memberService.updatePw(member));
+//		return null;
 //	}
-//	
-//}
+	
+
+	
+	@PostMapping("/join")
+	public ResponseEntity<?> join(@RequestBody Member member) throws Exception {
+		return new ResponseEntity<>(memberService.joinMember(member), HttpStatus.OK);
+	}
+	
+	@PostMapping("check")
+	public ResponseEntity<?> checkEmailId(@RequestBody Member member) throws Exception {
+		return new ResponseEntity<String>(memberService.checkEmailId(member), HttpStatus.OK);
+	}
+	
+	@GetMapping("pw")
+	public ResponseEntity<?> searchPw(
+			@PathVariable String emailId
+			) throws Exception {
+		return new ResponseEntity<Boolean>(memberService.searchPw(emailId), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> login(
+			@RequestBody Member member
+			) throws Exception {
+		return ResponseEntity.ok().body(memberService.login(member));
+	}
+	
+	@PutMapping("updatePw")
+	public ResponseEntity<?> updatePw(
+			@RequestBody Member member
+			) throws Exception {
+		return ResponseEntity.ok().body(memberService.updatePw(member));
+	}
+	
+}
--- backtest/src/main/java/com/ssafy/backtest/member/model/service/MemberService.java
+++ backtest/src/main/java/com/ssafy/backtest/member/model/service/MemberService.java
@@ -1,19 +1,19 @@
-//package com.ssafy.backtest.member.model.service;
-//
-//import com.ssafy.backtest.member.model.dto.Member;
-//
-//public interface MemberService {
-//
-//	Member getMember(String emailId) throws Exception;
-//
-//	boolean joinMember(Member member) throws Exception;
-//
-//	String login(Member member) throws Exception;
-//
-//	String checkEmailId(Member member) throws Exception;
-//
-//	boolean searchPw(String emailId) throws Exception;
-//
-//	boolean updatePw(Member member) throws Exception;
-//
-//}
+package com.ssafy.backtest.member.model.service;
+
+import com.ssafy.backtest.member.model.dto.Member;
+
+public interface MemberService {
+
+	Member getMember(String emailId) throws Exception;
+
+	boolean joinMember(Member member) throws Exception;
+
+	String login(Member member) throws Exception;
+
+	String checkEmailId(Member member) throws Exception;
+
+	boolean searchPw(String emailId) throws Exception;
+
+	boolean updatePw(Member member) throws Exception;
+
+}
